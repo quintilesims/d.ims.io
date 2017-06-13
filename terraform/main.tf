cf6e45e1c0316a0b7c4575d8eae3ebe358ee752e
zpatrick <zpatrick319@hotmail.com>
2017-06-13 22:36:27 +0000

    start terraform module

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_ecr_repository" "mod" {
+  name = "${var.ecr_repo_name}"
+}
+
+resource "aws_dynamodb_table" "mod" {
+  name           = "${var.dynamodb_table_name}"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "Token"
+
+  attribute {
+    name = "Token"
+    type = "S"
+  }
+}
+
+resource "aws_iam_user" "mod" {
+  name = "${var.iam_user_name}"
+}
+
+resource "aws_iam_access_key" "mod" {
+  user = "${aws_iam_user.mod.name}"
+}
+
+resource "aws_iam_user_policy" "mod" {
+  name   = "${aws_iam_user.mod.name}"
+  user   = "${aws_iam_user.mod.name}"
+  policy = "${data.template_file.user_policy.rendered}"
+}
+
+data "template_file" "user_policy" {
+  template = "${file("${path.module}/user_policy.json")}"
+
+  vars {
+    region = "${var.region}"
+    account_id = "${data.aws_caller_identity.current.account_id}"
+    repository = "${aws_ecr_repository.mod.name}"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+	default = "us-west-2"
+}
+
+variable "ecr_repo_name" {
+  default = "d.ims.io"
+}
+
+variable "dynamodb_table_name" {
+  default = "d.ims.io"
+}
+
+variable "iam_user_name" {
+  default = "d.ims.io"
+}
