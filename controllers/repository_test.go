11983262ee104ccdac2dbdbe5698ee575ddaf126
zpatrick <zpatrick319@hotmail.com>
2017-06-22 00:52:52 +0000

    add repository endpoints. add mocking for ecr

---
+++ controllers/repository_test.go
@@ -0,0 +1,108 @@
+package controllers
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ecr"
+	"github.com/golang/mock/gomock"
+	"github.com/quintilesims/d.ims.io/mock"
+	"github.com/quintilesims/d.ims.io/models"
+	"testing"
+)
+
+func TestCreateRepository(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockECR := mock.NewMockECRAPI(ctrl)
+	controller := NewRepositoryController(mockECR)
+
+	validateCreateRepositoryRequest := func(input *ecr.CreateRepositoryInput) {
+		if v, want := aws.StringValue(input.RepositoryName), "test"; v != want {
+			t.Errorf("Name was '%v', expected '%v'", v, want)
+		}
+	}
+
+	mockECR.EXPECT().
+		CreateRepository(gomock.Any()).
+		Do(validateCreateRepositoryRequest).
+		Return(&ecr.CreateRepositoryOutput{}, nil)
+
+	c := generateContext(t, models.CreateRepositoryRequest{Name: "test"}, nil)
+	if _, err := controller.CreateRepository(c); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDeleteRepository(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockECR := mock.NewMockECRAPI(ctrl)
+	controller := NewRepositoryController(mockECR)
+
+	validateDeleteRepositoryRequest := func(input *ecr.DeleteRepositoryInput) {
+		if v, want := aws.StringValue(input.RepositoryName), "test"; v != want {
+			t.Errorf("Name was '%v', expected '%v'", v, want)
+		}
+
+		if v, want := aws.BoolValue(input.Force), true; v != want {
+			t.Errorf("Force was '%v', expected '%v'", v, want)
+		}
+	}
+
+	mockECR.EXPECT().
+		DeleteRepository(gomock.Any()).
+		Do(validateDeleteRepositoryRequest).
+		Return(&ecr.DeleteRepositoryOutput{}, nil)
+
+	c := generateContext(t, nil, map[string]string{"name": "test"})
+	if _, err := controller.DeleteRepository(c); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetRepository(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockECR := mock.NewMockECRAPI(ctrl)
+	controller := NewRepositoryController(mockECR)
+
+	validateDescribeImagesRequest := func(input *ecr.DescribeImagesInput, fn func(*ecr.DescribeImagesOutput, bool) bool) {
+		if v, want := aws.StringValue(input.RepositoryName), "test"; v != want {
+			t.Errorf("Name was '%v', expected '%v'", v, want)
+		}
+	}
+
+	mockECR.EXPECT().
+		DescribeImagesPages(gomock.Any(), gomock.Any()).
+		Do(validateDescribeImagesRequest).
+		Return(nil)
+
+	c := generateContext(t, nil, map[string]string{"name": "test"})
+	if _, err := controller.GetRepository(c); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestListRepositories(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockECR := mock.NewMockECRAPI(ctrl)
+	controller := NewRepositoryController(mockECR)
+
+	validateDescribeRepositoriesRequest := func(input *ecr.DescribeRepositoriesInput, fn func(*ecr.DescribeRepositoriesOutput, bool) bool) {
+		// no fields to validate
+	}
+
+	mockECR.EXPECT().
+		DescribeRepositoriesPages(gomock.Any(), gomock.Any()).
+		Do(validateDescribeRepositoriesRequest).
+		Return(nil)
+
+	c := generateContext(t, nil, nil)
+	if _, err := controller.ListRepositories(c); err != nil {
+		t.Fatal(err)
+	}
+}
