c79e79cca8bbed1926c1716c7ce743df4e5dce18
zpatrick <zpatrick319@hotmail.com>
2017-06-22 19:03:37 +0000

    use AuthenticatorFunc for composite auth

--- auth/composite_authenticator.go
+++ auth/composite_authenticator.go
@@ -1,26 +1,18 @@
 package auth
 
-type CompositeAuthenticator struct {
-	authenticators []Authenticator
-}
+func NewCompositeAuthenticator(authenticators ...Authenticator) AuthenticatorFunc {
+	return AuthenticatorFunc(func(user, pass string) (bool, error) {
+		for _, authenticator := range authenticators {
+			isValid, err := authenticator.Authenticate(user, pass)
+			if err != nil {
+				return false, err
+			}
 
-func NewCompositeAuthenticator(authenticators ...Authenticator) *CompositeAuthenticator {
-	return &CompositeAuthenticator{
-		authenticators: authenticators,
-	}
-}
-
-func (c *CompositeAuthenticator) Authenticate(user, pass string) (bool, error) {
-	for _, authenticator := range c.authenticators {
-		isValid, err := authenticator.Authenticate(user, pass)
-		if err != nil {
-			return false, err
+			if isValid {
+				return true, nil
+			}
 		}
 
-		if isValid {
-			return true, nil
-		}
-	}
-
-	return false, nil
+		return false, nil
+	})
 }
