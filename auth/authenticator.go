5bf8f288dc92d4e23b7aa3d221ea562b263f2c59
zpatrick <zpatrick319@hotmail.com>
2017-06-22 01:22:25 +0000

    add tests for composite authenticator

--- auth/authenticator.go
+++ auth/authenticator.go
@@ -3,3 +3,9 @@ package auth
 type Authenticator interface {
 	Authenticate(user, pass string) (bool, error)
 }
+
+type AuthenticatorFunc func(string, string) (bool, error)
+
+func (a AuthenticatorFunc) Authenticate(user, pass string) (bool, error) {
+	return a(user, pass)
+}
