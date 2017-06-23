f93f1e047cf4e9ac52de4b56aa818776ce6f80a4
zpatrick <zpatrick319@hotmail.com>
2017-06-23 21:36:13 +0000

    add terraform module

---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "dynamodb_table_name" {
+  default = "d.ims.io"
+}
+
+variable "iam_user_name" {
+  default = "d.ims.io"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "environment_id" {
+  description = "ID of the Layer0 environment to build the service"
+}
+
+variable "load_balancer_name" {
+  description = "Name of the Layer0 load balancer to create"
+  default     = "dimsio"
+}
+
+variable "service_name" {
+  description = "Name of the Layer0 service to create"
+  default     = "dimsio"
+}
+
+variable "deploy_name" {
+  description = "Name of the Layer0 deploy to create"
+  default     = "dimsio"
+}
+
+variable "scale" {
+  description = "The scale of the service"
+  default     = 1
+}
+
+variable "certificate_name" {
+  description = "The name of the certificate to use on the load balancer"
+}
+
+variable "docker_image" {
+  description = "Docker image to use"
+  default     = "quintilesims/d.ims.io:latest"
+}
