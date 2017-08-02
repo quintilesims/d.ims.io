f1c47843ddaad3cfc9bd010bd6dcd771f35470bd
zpatrick <zpatrick319@hotmail.com>
2017-08-02 20:07:42 +0000

    change image model. return list of strings for get repo images

--- models/image.go
+++ models/image.go
@@ -7,22 +7,18 @@ import (
 )
 
 type Image struct {
-	Repository    string    `json:"repository"`
-	ImageTags     []string  `json:"image_tags,omitempty"`
-	ImageDigest   string    `json:"image_digest"`
-	ImagePushedAt time.Time `json:"image_pushed_at"`
-	ImageSize     int64     `json:"image_size"`
+	Digest   string    `json:"digest"`
+	Size     string    `json:"size"`
+	PushedAt time.Time `json:"pushed_at"`
 }
 
 func (r Image) Definition() swagger.Definition {
 	return swagger.Definition{
 		Type: "object",
 		Properties: map[string]swagger.Property{
-			"repository":      swagger.NewStringProperty(),
-			"image_tags":      swagger.NewStringSliceProperty(),
-			"image_digest":    swagger.NewStringProperty(),
-			"image_pushed_at": swagger.NewStringProperty(),
-			"image_size":      swagger.NewIntProperty(),
+			"digest":    swagger.NewStringProperty(),
+			"size":      swagger.NewStringProperty(),
+			"pushed_at": swagger.NewStringProperty(),
 		},
 	}
 }
