96db6a6a66ce9ad83b0e3dee4b96aa3c8831c0c6
Zack Patrick <zpatrick319@hotmail.com>
2018-05-17 21:26:54 -0700

    Update router.go

--- router/router.go
+++ router/router.go
@@ -8,7 +8,6 @@ import (
 
 func NewRouter(routes []*fireball.Route, doProxy fireball.Handler) fireball.RouterFunc {
 	router := fireball.NewBasicRouter(routes)
-
 	return fireball.RouterFunc(func(req *http.Request) (*fireball.RouteMatch, error) {
 		match, err := router.Match(req)
 		if match != nil || err != nil {
