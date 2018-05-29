e567fa9c536dc49d076e8522408fdf0555ce3c99
Zack Patrick <zpatrick319@Hotmail.com>
2018-05-29 13:04:47 -0700

    fix swagger doc delete tag

--- controllers/swagger.go
+++ controllers/swagger.go
@@ -183,7 +183,7 @@ func (s *SwaggerController) serveSwaggerJSON(c *fireball.Context) (fireball.Resp
 					Parameters: []swagger.Parameter{
 						swagger.NewStringPathParam("owner", "Owner of the Repository", true),
 						swagger.NewStringPathParam("name", "Name of the Repository", true),
-						swagger.NewStringPathParam("tag", "Tag of the Image to delete", true),
+						swagger.NewStringPathParam("tag", "Tag of the Image to view", true),
 					},
 					Security: swagger.BasicAuthSecurity("login"),
 					Responses: map[string]swagger.Response{
